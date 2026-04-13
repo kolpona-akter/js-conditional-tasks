72ad7b397c7cf127156fcb92ff6072740262c195
Mst Surnaly Akter <surnalyms@gmail.com>
2026-04-13 21:17:30 +0600

    added task-1

---
+++ task-1.js
@@ -0,0 +1,24 @@
+// let burgerPrice = 450;
+// // let cokePrice;
+
+// if(burgerPrice > 500){
+//     console.log('free coke')
+// }
+// else{
+//     console.log('coke price 30tk')
+// }
+
+
+let burgerPrice = 550;
+let cokePrice;
+
+if(burgerPrice > 500){
+    cokePrice = 0;   //free coke
+}
+else{
+    cokePrice = 30;   //coke price 30tk
+}
+
+
+console.log("Burger Price:",burgerPrice);
+console.log("Coke Price:", cokePrice);
---
+++ task-2.js
@@ -0,0 +1,21 @@
+let weight = 36;  //kg
+let height = 1.524; //meter
+
+ let BMI = weight / (height * height);
+
+ console.log("Your BMI is:"+ BMI.toFixed(3));
+ //template string
+  //console.log(`Your BMI is: ${BMI.toFixed(2)}`);
+
+if(BMI < 18.5){
+    console.log('you are underweight');
+}
+else if(BMI >= 18.5 && BMI <= 24.9){
+    console.log('you are normal');
+}
+else if(BMI >= 25 && BMI <= 29.9){
+    console.log('you are overweight')
+}
+else{
+    console.log('you are obese')
+}
